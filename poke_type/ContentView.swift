305581e6e5b8c2ab028ab2622e0777d91ceae15c
momoko0529 <karta43215@gmail.com>
2019-11-11 16:23:31 +0800

    Initial Commit

---
+++ poke_type/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  poke_type
+//
+//  Created by User14 on 2019/11/11.
+//  Copyright © 2019 com.pika. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
